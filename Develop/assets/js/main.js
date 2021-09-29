bfed0730fd6d28d924b38331425da5d308683f14
ChristianAnti <89366348+ChristianAnti@users.noreply.github.com>
2021-09-29 00:28:50 -0400

    added questions that will be used and basic html and css

--- Develop/assets/js/main.js
+++ Develop/assets/js/main.js
@@ -1,11 +1,16 @@
 var startButton = document.querySelector("#start-button");
+var questions = [];
+var score = 0;
 
-
+//questions 1. What does CSS stand for? A:Colorful StyleSheet B:Creative Style Sheet C:Cascading Style Sheets D:Computer Style Sheets (Answer) C: C:Cascading Style Sheets
+//questions 2. What does HTML stand for? A:Hyperlinks and text markup language B:Hyper text markup language C:Hyper text making language D:Hyper text mark language (Answer)B: Hypertext Markup Language
+//questions 3. Which HTML tag is used to define an internal styles sheet? A:script B:style C:html D:svg (Answer)B: style
+//questions 4. Which of these is a comment in Javascript? A:<!--Comment--> B://Comment// C:/*Comment*/ D://Comment (Answer)D: //Comment
 console.log("I'm connected WOW");
 
-var imgTag = document.createElement("img");
 
-
+// Base code commented out unless I need it 
+// var imgTag = document.createElement("img");
 // imgTag.setAttribute("src", "assets/images/hoopla-is-dead.png");
 // imgTag.setAttribute("alt", "Start Test");
 // document.body.appendChild(imgTag);
@@ -20,28 +25,25 @@ startButton.addEventListener("click", function(){
 // GIVEN I am taking a code quiz
 
 // WHEN I click the start button
-
 // THEN a timer starts and I am presented with a question
 
 
 
 // WHEN I answer a question
-
 // THEN I am presented with another question
 
-// WHEN I answer a question incorrectly
 
+
+// WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
 
 
 
 // WHEN all questions are answered or the timer reaches 0
-
 // THEN the game is over
 
 
 
 // WHEN the game is over
-
 // THEN I can save my initials and set my score on the board
 
